ff5c1f918ff7513d13ab23b5d6b0da040d70b0d1
Patrycja Zawadka <patrycja@figment.io>
2021-03-05 16:59:28 +0100

    Stash and unvested event

--- mapper/event_mapper.go
+++ mapper/event_mapper.go
@@ -124,7 +124,7 @@ func (e *event) parseEventDescription(log *zap.SugaredLogger, ev *eventpb.Event)
 			e.eventType = []string{"error"}
 		case "info", "tip_hash", "call_hash", "index":
 			break
-		case "from", "stash", "unvested":
+		case "from":
 			e.senderAccountID, err = getAccountID(ev.Data[i])
 		case "to", "who":
 			e.recipientAccountID, err = getAccountID(ev.Data[i])
